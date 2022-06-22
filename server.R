f485bdcaa9da12836087091b7ff1aff740353101
Muhd Humam Rhamadhani <63768487+EmHaEr@users.noreply.github.com>
2022-06-23 00:22:13 +0700

    Update 1

--- server.R
+++ server.R
@@ -2,15 +2,6 @@ server <- function(input, output, session)
 {
   bs_themer()
   
-  output$shiny <- renderUI(
-    {
-      source("shiny.R")
-      
-      callModule(server_shiny, id = "shiny")
-      
-      ui_shiny(id = "shiny")
-    })
-  
     data <- reactive(
     {
       upload_data <- input$upload_data
@@ -75,9 +66,6 @@ server <- function(input, output, session)
       print(data_selected)
     })
   
-  
-
-  
   output$download_data <- downloadHandler(
     filename = function()
     {
@@ -90,5 +78,206 @@ server <- function(input, output, session)
       write.csv(data_selectizeinput(),file)
     }
   )
+  
+  #############################
+  ########### PCA #############
+  #############################
+  dataset <- reactive(
+    {
+      upload_data <- input$upload_data
+      
+      if(is.null(upload_data))
+        return(NULL)
+      
+      data = read.csv(upload_data$datapath, sep = input$pemisah_variabel, 
+                      row.names = 1)
+      return(data)
+    })
+  
+  output$tampilan_dataset <- DT::renderDT(
+    {
+      data_terpilih <- dataset()
+      
+      DT::datatable(data_terpilih)
+    })
+  
+  output$profil_dataset <- renderPrint(
+    {
+      profil_dataset <- dataset()
+      
+      glimpse(profil_dataset)
+    })
+  
+  output$summary_dataset <- renderPrint(
+    {
+      summary_dataset <- dataset()
+      
+      summary(summary_dataset)
+      skim(summary_dataset)
+    })
+  
+  output$hist_dataset <- renderPlot(
+    {
+      hist_dataset <- dataset()
+      
+      vis_miss(hist_dataset)
+      vis_dat(hist_dataset)
+      
+      par(mfrow=c(3,4))
+      for(i in 1:ncol(hist_dataset)) {
+        hist(hist_dataset[, i], main = paste(colnames(hist_dataset[i])), xlab = "")
+      }
+    })
+  
+  output$hasil_modelPCA <- DT::renderDT(
+    {
+      hasil_modelPCA <- dataset()
+      
+      pcaModel <- prcomp(hasil_modelPCA, scale. = TRUE, center = TRUE)
+      pcaModel$rotation
+    })
+  
+  output$hasil_summaryPCA <- renderPrint(
+    {
+      hasil_summaryPCA <- dataset()
+      
+      pcaModel <- prcomp(hasil_summaryPCA, scale. = TRUE, center = TRUE)
+      pcaModel$rotation
+      
+      summary(pcaModel)
+    })
+  
+  output$hasil_predictPCA <- DT::renderDT(
+    {
+      hasil_predictPCA <- dataset()
+      
+      pcaModel <- prcomp(hasil_predictPCA, scale. = TRUE, center = TRUE)
+      pcaModel$rotation
+      
+      predict_PCA=predict(pcaModel, newdata=hasil_predictPCA)
+    })
+  
+  output$eigenvalue_PCA <- renderPrint(
+    {
+      eigenvalue_PCA <- dataset()
+      
+      modelPCA <- prcomp(eigenvalue_PCA, scale. = TRUE, center = TRUE)
+      modelPCA$rotation
+      
+      get_eigenvalue(modelPCA)
+    })
+  
+  output$coord_PCA <- renderPrint(
+    {
+      coord_PCA <- dataset()
+      
+      modelPCA <- prcomp(coord_PCA, scale. = TRUE, center = TRUE)
+      modelPCA$rotation
+      
+      var <- get_pca_var(modelPCA)
+      var
+      
+      head(var$coord)
+    })
+  
+  output$cos2_PCA <- renderPrint(
+    {
+      cos2_PCA <- dataset()
+      
+      modelPCA <- prcomp(cos2_PCA, scale. = TRUE, center = TRUE)
+      modelPCA$rotation
+      
+      var <- get_pca_var(modelPCA)
+      var
+      
+      head(var$cos2)
+    })
+  
+  output$contrib_PCA <- renderPrint(
+    {
+      contrib_PCA <- dataset()
+      
+      modelPCA <- prcomp(contrib_PCA, scale. = TRUE, center = TRUE)
+      modelPCA$rotation
+      
+      var <- get_pca_var(modelPCA)
+      var
+      
+      head(var$contrib)
+    })
+  
+  output$screeplot_PCA <- renderPlot(
+    {
+      screeplot_PCA <- dataset()
+      
+      modelPCA <- prcomp(screeplot_PCA, scale. = TRUE, center = TRUE)
+      modelPCA$rotation
+      
+      fviz_eig(modelPCA)
+    })
+  
+  output$cos2plot_PCA <- renderPlot(
+    {
+      cos2plot_PCA <- dataset()
+      
+      modelPCA <- prcomp(cos2plot_PCA, scale. = TRUE, center = TRUE)
+      modelPCA$rotation
+      
+      fviz_cos2(modelPCA, choice = "var", axes = 1:2)
+    })
+  
+  output$individuals_PCA <- renderPlot(
+    {
+      biplot_PCA <- dataset()
+      
+      modelPCA <- prcomp(biplot_PCA, scale. = TRUE, center = TRUE)
+      modelPCA$rotation
+      
+      fviz_eig(modelPCA)
+      
+      fviz_pca_ind(modelPCA, repel = TRUE,
+                   col.ind = "cos2",
+                   gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+      )
+    })
+  
+  output$contribplot_PCA <- renderPlot(
+    {
+      contribplot_PCA <- dataset()
+      
+      modelPCA <- prcomp(contribplot_PCA, scale. = TRUE, center = TRUE)
+      modelPCA$rotation
+      
+      fviz_contrib(modelPCA, choice = "var", axes = 1:2, top = 10)
+    })
+  
+  output$variables_PCA <- renderPlot(
+    {
+      variables_PCA <- dataset()
+      
+      modelPCA <- prcomp(variables_PCA, scale. = TRUE, center = TRUE)
+      modelPCA$rotation
+      
+      fviz_eig(modelPCA)
+      
+      fviz_pca_var(modelPCA, repel = TRUE,
+                   col.var = "contrib",
+                   gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+      )
+    })
+  
+  output$biplot_PCA <- renderPlot(
+    {
+      biplot_PCA <- dataset()
+      
+      modelPCA <- prcomp(biplot_PCA, scale. = TRUE, center = TRUE)
+      modelPCA$rotation
+      
+      fviz_eig(modelPCA)
+      
+      fviz_pca_biplot(modelPCA, repel = TRUE,
+                      col.var = "blue",
+                      col.ind = "#000000")
+    })
   
 }
--- ui.R
+++ ui.R
@@ -4,8 +4,6 @@ library(shinyWidgets)
 library(shinydashboard)
 library(shinycssloaders)
 library(shinythemes)
-library(e1071)
-library(caret)
 library(caTools)
 library(maps)
 library(leaflet)
@@ -15,33 +13,20 @@ library(DT)
 library(stargazer)
 library(bslib)
 library(rsconnect)
+#Library Dataset
+library(dplyr)
+library(skimr)
+library(visdat)
+#Library PCA
+library(factoextra)
+library(corrplot)
+library(ggplot2)
 
 thematic::thematic_shiny(font = "auto")
 
 ui <- fluidPage(
     theme = bs_theme(),
-    #Judul Aplikasi
-    h1("Aplikasi Data Analisis Menggunakan Metode PCA", style="
-    font-family: 'cursive';
-    font-weight: bold;
-    font-size:50px;
-    text-align:center
-    "),
-    
-    #R Shiny
-    HTML('<center><img src="rshiny.png" width="200"></center>'),
-    br(),
-    
-    #Logo UII
-    HTML('<center><img src="UII.png" width="100" height="150"></center>'),
-    br(),
 
-    #Credit
-    h2("Created by : Muhd Humam Rhamadhani", style="
-    font-family: 'cursive';
-    font-size:20px;
-    text-align:center
-    "),
     
     #Output
     h2("Input Data"),
@@ -70,6 +55,8 @@ ui <- fluidPage(
             DT::DTOutput("data2")
         )
     ),
+    hr(),
+    
     h2("Data Processing (Pemilihan Variabel)"),
     sidebarLayout(
         sidebarPanel(
@@ -83,8 +70,128 @@ ui <- fluidPage(
             DT::DTOutput("data_selectizeinput"),
         )
     ),
-    br(),
+    hr(),
+    
     h2("PCA"),
-    uiOutput("shiny"),
-    br()
+    fluidPage(
+        navlistPanel(
+            #Tab Panel Upload Data
+            tabPanel("Upload",
+                     h3("Analisis data menggunakan metode PCA", 
+                        style="
+                  font-family:'arial'
+                  color:black;
+                  text-align:center"
+                     ),
+                     br(),
+                     sidebarPanel(
+                         fileInput("upload_data", "Choose .txt/.csv file",
+                                   accept = c("text/csv",
+                                              "text/comma-separated-values",
+                                              ".csv"))
+                     ),
+            ),
+            #Tab Panel Dataset
+            tabPanel("Dataset",
+                     radioButtons("pemisah_variabel", "Separator",
+                                  choices = c(Comma = ",",
+                                              Semicolon = ";",
+                                              Tab = "\t"),
+                                  selected = ",", inline = TRUE),
+                     h3("Tampilan Dataset", style=
+                            "text-align:center"),
+                     DT::DTOutput("tampilan_dataset"),
+                     hr(),
+                     h3("Profil Dataset", style=
+                            "text-align:center"),
+                     verbatimTextOutput("profil_dataset"),
+                     hr(),
+                     h3("Summary Dataset", style=
+                            "text-align:center"),
+                     verbatimTextOutput("summary_dataset"),
+                     hr(),
+                     h3("Histogram Dataset", style=
+                            "text-align:center"),
+                     plotOutput("hist_dataset")
+            ),
+            #Tab Panel PCA
+            tabPanel("PCA",
+                     h3("Eigenvalue", style=
+                            "text-align:center"),
+                     verbatimTextOutput("eigenvalue_PCA"),
+                     h5("tes",
+                        style= "text-align:center"),
+                     hr(),
+                     h3("Coordinates", style=
+                            "text-align:center"),
+                     verbatimTextOutput("coord_PCA"),
+                     h5("tes",
+                        style= "text-align:center"),
+                     hr(),
+                     h3("Cos 2", style=
+                            "text-align:center"),
+                     verbatimTextOutput("cos2_PCA"),
+                     h5("tes",
+                        style= "text-align:center"),
+                     hr(),
+                     h3("Contribution", style=
+                            "text-align:center"),
+                     verbatimTextOutput("contrib_PCA"),
+                     h5("tes",
+                        style= "text-align:center"),
+                     hr(),
+                     h3("Summary", style=
+                            "text-align:center"),
+                     verbatimTextOutput("hasil_summaryPCA"),
+                     h5("tes",
+                        style= "text-align:center"),
+                     hr(),
+                     h3("Model", style=
+                            "text-align:center"),
+                     DT::DTOutput("hasil_modelPCA"),
+                     h5("tes",
+                        style= "text-align:center"),
+                     hr(),
+                     h3("Predict", style=
+                            "text-align:center"),
+                     DT::DTOutput("hasil_predictPCA"),
+                     h5("tes",
+                        style= "text-align:center"),
+            ),
+            #Tab Panel Visualisasi PCA
+            tabPanel("Visualisasi",
+                     h3("Scree Plot", style=
+                            "text-align:center"),
+                     plotOutput("screeplot_PCA"),
+                     h5("Menggambarkan nilai dari Eigen Values tiap variabel ke dalam bentuk dimensi plot",
+                        style= "text-align:center"),
+                     hr(),
+                     h3("Cos2 Plot", style=
+                            "text-align:center"),
+                     plotOutput("cos2plot_PCA"),
+                     h3("Individuals Plot", style=
+                            "text-align:center"),
+                     plotOutput("individuals_PCA"),
+                     h5("Menggambarkan nilai dari cos2 ke tiap dimensi plot",
+                        style= "text-align:center"),
+                     hr(),
+                     h3("Contrib Plot", style=
+                            "text-align:center"),
+                     plotOutput("contribplot_PCA"),
+                     h3("Variables Plot", style=
+                            "text-align:center"),
+                     plotOutput("variables_PCA"),
+                     h5("Menggambarkan nilai dari contrib sebagai kontribusi PCA ke tiap dimensi plot",
+                        style= "text-align:center"),
+                     hr(),
+                     h3("Biplot", style=
+                            "text-align:center"),
+                     plotOutput("biplot_PCA"),
+                     
+                     h5("Menggambarkan gabungan dari Individuals Plot dan Variables Plot",
+                        style= "text-align:center"),
+            )
+        ) #navlistPanel
+    ), #fluidPage
+    hr()
 )
